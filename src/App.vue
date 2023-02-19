601788dde118ae491238c3f038d9df64a2e5f906
NuRoZ123 <killiangouzien44220@gmail.com>
2023-02-19 21:59:15 +0100

    login OK

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,24 @@ import Login from './components/Login.vue'
 import Accueil from './components/Accueil.vue'
 
 const tokenExist = localStorage.getItem("token");
-const connexionDate = new Date(localStorage.getItem("connexionDate"));
-connexionDate.setHours(connexionDate.getHours() + 3);
-const now = new Date(Date.now().valueOf());
-const isMoreThan3Hours = connexionDate > now;
+if(tokenExist) {
+  const connexionDate = new Date(parseInt(localStorage.getItem("connexionDate")));
 
+  connexionDate.setHours(connexionDate.getHours() + 3);
+  const now = new Date(Date.now().valueOf());
+  const isMoreThan3Hours = connexionDate > now;
+
+  if(!isMoreThan3Hours) {
+    localStorage.removeItem("token");
+    localStorage.removeItem("connexionDate");
+    window.location.reload();
+  }
+
+}
 </script>
 
 <template>
-  <Accueil v-if="$route.name === 'accueil' && tokenExist && !isMoreThan3Hours"/>
+  <Accueil v-if="$route.name === 'accueil' && tokenExist"/>
   <Login v-else-if="$route.name === 'login' || !tokenExist" />
 
 </template>
